2fce129f6a35272ddca53e2f18a95a6faea5a9ef
baojingh <baojingh@163.com>
2021-01-09 10:49:34 +0800

    add: shiro integration

---
+++ src/main/java/com/bd/thinkinginspring/config/ShiroConfig.java
@@ -0,0 +1,15 @@
+package com.bd.thinkinginspring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: baojing.he
+ * @Date: 2021/1/8 22:28
+ * @Description:
+ */
+
+@Configuration
+public class ShiroConfig {
+    
+
+}
